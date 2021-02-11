280f449da71e032fce9fe1f91d24456c24de4f75
Hans Adrianus <hans.adrianus@outlook.com>
2021-02-12 02:49:41 +0700

    Initial Commit
    Initial Commit

---
+++ resources/views/area/faq.blade.php
@@ -0,0 +1,102 @@
+<!-- Faq area start -->
+<div class="faq-area area-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="section-headline text-center">
+                    <h2>Faq Question</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="faq-details">
+                    <div class="panel-group" id="accordion">
+                        <!-- Panel Default -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="check-title">
+                                    <a data-toggle="collapse" class="active" data-parent="#accordion" href="#check1">
+                                        <span class="acc-icons"></span>Consectetur adipisicing elit.
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="check1" class="panel-collapse collapse in">
+                                <div class="panel-body">
+                                    <p>
+                                        Redug Lefes dolor sit amet, consectetur adipisicing elit. Aspernatur, tempore,
+                                        commodi quas mollitia dolore magnam quidem repellat, culpa voluptates laboriosam
+                                        maiores alias accusamus recusandae vero
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Panel Default -->
+                        <!-- Panel Default -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="check-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#check2">
+                                        <span class="acc-icons"></span> Dolore magnam quidem repellat.
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="check2" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                    <p>
+                                        Redug Lefes dolor sit amet, consectetur adipisicing elit. Aspernatur, tempore,
+                                        commodi quas mollitia dolore magnam quidem repellat, culpa voluptates laboriosam
+                                        maiores alias accusamus recusandae vero aperiam sint nulla beatae eos.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Panel Default -->
+                        <!-- Panel Default -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="check-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#check3">
+                                        <span class="acc-icons"></span>Redug Lefes dolor sit.
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="check3" class="panel-collapse collapse ">
+                                <div class="panel-body">
+                                    <p>
+                                        Redug Lefes dolor sit amet, consectetur adipisicing elit. Aspernatur, tempore,
+                                        commodi quas mollitia dolore magnam quidem repellat, culpa voluptates laboriosam
+                                        maiores alias accusamus recusandae vero aperiam sint nulla beatae eos.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Panel Default -->
+                        <!-- Panel Default -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="check-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#check4">
+                                        <span class="acc-icons"></span>Maiores alias accusamus
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="check4" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                    <p>
+                                        Redug Lefes dolor sit amet, consectetur adipisicing elit. Aspernatur, tempore,
+                                        commodi quas mollitia dolore magnam quidem repellat, culpa voluptates laboriosam
+                                        maiores alias accusamus recusandae vero aperiam sint nulla beatae eos.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Panel Default -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end Row -->
+    </div>
+</div>
+<!-- End Faq Area -->
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <title>My Home Spaceconomic</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicons -->
+    <link href="{{ asset('img/favicon.png') }}" rel="icon">
+    <link href="{{ asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700|Raleway:300,400,400i,500,500i,700,800,900"
+        rel="stylesheet">
+
+    <!-- Bootstrap CSS File -->
+    <link href="{{ asset('lib/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- Libraries CSS Files -->
+    <link href="{{ asset('lib/nivo-slider/css/nivo-slider.css') }}" rel="stylesheet">
+    <link href="{{ asset('lib/owlcarousel/owl.carousel.css') }}" rel="stylesheet">
+    <link href="{{ asset('lib/owlcarousel/owl.transitions.css') }}" rel="stylesheet">
+    <link href="{{ asset('lib/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('lib/venobox/venobox.css') }}" rel="stylesheet">
+
+    <!-- Nivo Slider Theme -->
+    <link href="{{ asset('css/nivo-slider-theme.css') }}" rel="stylesheet">
+
+    <!-- Main Stylesheet File -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <!-- Responsive Stylesheet File -->
+    <link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
+</head>
+
+<body data-spy="scroll" data-target="#navbar-example">
+
+    <div id="preloader"></div>
+
+    @include('layout.header')
+
+    @include('area.home')
+    @include('area.about')
+    @include('area.service')
+    @include('area.faq')
+    @include('area.review')
+    @include('area.portfolio')
+    @include('area.pricing')
+    @include('area.testimony')
+    @include('area.order')
+    @include('area.contact')
+
+    @include('layout.footer')
+
+    <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+    <!-- JavaScript Libraries -->
+    <script src="{{ asset('lib/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('lib/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('lib/owlcarousel/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('lib/venobox/venobox.min.js') }}"></script>
+    <script src="{{ asset('lib/knob/jquery.knob.js') }}"></script>
+    <script src="{{ asset('lib/wow/wow.min.js') }}"></script>
+    <script src="{{ asset('lib/parallax/parallax.js') }}"></script>
+    <script src="{{ asset('lib/easing/easing.min.js') }}"></script>
+    <script src="{{ asset('lib/nivo-slider/js/jquery.nivo.slider.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('lib/appear/jquery.appear.js') }}"></script>
+    <script src="{{ asset('lib/isotope/isotope.pkgd.min.js') }}"></script>
+
+    <!-- Contact Form JavaScript File -->
+    <script src="{{ asset('contactform/contactform.js') }}"></script>
+
+    <script src="{{ asset('js/main.js') }}"></script>
+  </body>
+
+</html>
