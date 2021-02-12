a8ce86d07a77bb313916a0291afe53156f405866
Hans Adrianus <hans.adrianus@outlook.com>
2021-02-12 21:45:01 +0700

    modify review page

--- resources/views/area/review.blade.php
+++ resources/views/area/review.blade.php
@@ -6,12 +6,14 @@
                 <img src="{{ asset('img/about/2.jpg') }}" alt="">
             </a>
         </div>
-        <div class="ml-2 mr-2 work-right-text text-center">
+        <div class="work-right-text text-center">
             <h2>MY INSPIRATION IS REAL</h2>
-            <h5>Kini apa yang pernah anda bayangkan, impikan dan inspirasikan sudah dapat terwujud. </h5>
-            <h5>Tidak hanya interior anda saja yang nyaman,
-            property anda pun akan semakin bernilai.</h5>
-            
+            <div style="margin-left: 25px; margin-right: 25px;">
+                <h5>Kini apa yang pernah anda bayangkan, impikan dan inspirasikan sudah dapat terwujud. </h5>
+                <h5>Tidak hanya interior anda saja yang nyaman,
+                    property anda pun akan semakin bernilai.</h5>
+            </div>
+
         </div>
     </div>
 </div>
